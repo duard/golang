852ce35c35c3e8d49108b96cb16fdfd06804c63c
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-26 16:06:11 -0300

    arrays

--- 04/2/04-go_test.go
+++ 04/2/04-go_test.go
@@ -15,4 +15,15 @@ func TestAdd(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+
+	t.Run("Another size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Add(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
